45846c8723f1bfc3975e6930f5ffdc8b7e2cfc7e
Madinab99999 <bimakanova9@mail.ru>
2024-10-20 11:21:06 +0500

    Update move.go

--- bot/handler/move.go
+++ bot/handler/move.go
@@ -25,7 +25,7 @@ func (h *Handler) Move(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	position := calculateMove(req.Board, req.Token)
+	position := ticTacToe.CalculateBestMove(req.Board, req.Token)
 	resp := ResponseMove{Index: position}
 
 	w.Header().Set("Content-Type", "application/json")
@@ -33,70 +33,4 @@ func (h *Handler) Move(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "failed to respond to move", http.StatusInternalServerError)
 		log.Printf("Failed to encode response: %v", err)
 	}
-}
-
-func calculateMove(board *ticTacToe.Board, token ticTacToe.Token) int {
-
-	for i, cell := range board {
-		if cell == ticTacToe.TokenEmpty {
-			board[i] = token
-			if checkWinner(board, token) {
-				board[i] = ticTacToe.TokenEmpty
-				return i
-			}
-			board[i] = ticTacToe.TokenEmpty
-		}
-	}
-
-	opponent := getOpponentToken(token)
-	for i, cell := range board {
-		if cell == ticTacToe.TokenEmpty {
-			board[i] = opponent
-			if checkWinner(board, opponent) {
-				board[i] = ticTacToe.TokenEmpty
-				return i
-			}
-			board[i] = ticTacToe.TokenEmpty
-		}
-	}
-
-	if board[4] == ticTacToe.TokenEmpty {
-		return 4
-	}
-
-	for _, i := range []int{0, 2, 6, 8} {
-		if board[i] == ticTacToe.TokenEmpty {
-			return i
-		}
-	}
-
-	for i, cell := range board {
-		if cell == ticTacToe.TokenEmpty {
-			return i
-		}
-	}
-
-	return -1
-}
-
-func checkWinner(board *ticTacToe.Board, token ticTacToe.Token) bool {
-	winPatterns := [][3]int{
-		{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // Rows
-		{0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // Columns
-		{0, 4, 8}, {2, 4, 6}, // Diagonals
-	}
-
-	for _, pattern := range winPatterns {
-		if board[pattern[0]] == token && board[pattern[1]] == token && board[pattern[2]] == token {
-			return true
-		}
-	}
-	return false
-}
-
-func getOpponentToken(token ticTacToe.Token) ticTacToe.Token {
-	if token == ticTacToe.TokenX {
-		return ticTacToe.TokenO
-	}
-	return ticTacToe.TokenX
 }
